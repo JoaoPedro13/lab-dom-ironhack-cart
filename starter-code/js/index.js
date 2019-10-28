d2ed964661f2d7fab4572e80aa4a1bfe9724fd6b
João Pedro Caldeira <jpc130@gmail.com>
2019-10-28 16:48:18 +0000

    missing the last bonus iteration

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,15 +1,36 @@
 // ITERATION 1
 
-function updateSubtotal($product) {
-  // ...
-}
 
-function calculateAll() {
-  // ITERATION 2
-  //...
+
+//console.log ($product[0].querySelector('.price span').innerText)
+function updateSubtotal($product){
+  for (let i=0 ; i<$product.length; i++){
+    let $price = parseFloat($product[i].querySelector('.price span').innerText);
+    let $quantity = $product[i].querySelector('.quantity input').valueAsNumber;
+    let $subtotal = $product[i].querySelector('.subtotal span');
+
+  $subtotal.innerText = $quantity * $price
+  }
+}
+ 
+// ITERATION 2
+function calculateAll() { 
+  
+  const $product = document.getElementsByClassName('product');
+  let total= 0
+   
+  for (let i=0; i<$product.length; i++){
+    updateSubtotal($product)
+    let subtotal = parseInt($product[i].querySelector('.subtotal span').innerText)
+    total += subtotal;
+  }
+    document.querySelector('#total-value span').innerText = total;
+  }
+  
+
   // ITERATION 3
   //...
-}
+
 
 const $calculateTrigger = document.getElementById('calculate');
 
@@ -18,11 +39,16 @@ $calculateTrigger.addEventListener('click', calculateAll);
 // ITERATION 4
 
 function addProductRemoveListener($removeButton) {
-  // ...
-}
+  let = $removebutton = document.getElementsByClassName('btn-remove');
+  for (let i = 0; i < $removebutton.length; i++) {
 
+    $removebutton[i].addEventListener('click', a => {
+      event.currentTarget.parentNode.parentNode.remove();
+    })
+  }
+}
 // ITERATION 5
 
 function createProduct(event) {
-  // ...
+
 }
